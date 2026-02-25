840e8bad7b7a190c7f154c42cdc97e67025af64d
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-02-25 11:30:58 -0300

    add: clase al input search e icono lupa

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -35,7 +35,10 @@ const Aside = () => {
     return(
         <aside>
             <h1>Chat UTN</h1>
-            <input type="search" placeholder="Buscar un chat" onChange={handleChange}/>
+            <div className="search-bar">
+                <svg class="icon"  xmlns="http://www.w3.org/2000/svg" width="128" height="128" viewBox="0 0 32 32"><path fill="none" stroke="#cccccc" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 27l7.5-7.5M28 13a9 9 0 1 1-18 0a9 9 0 0 1 18 0"/></svg>
+                <input type="search" placeholder="Buscar un chat" onChange={handleChange}/>
+            </div>
             {
                 filteredUsers.length === 0 && <p>No se encontraron resultados.</p>
             }
