be75f21476f074ffd4073b8bde9c1095286c1336
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-02-27 10:17:55 -0300

    add: ruta del Login

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "../views/Home"
 import { NotFound } from "../views/NotFound"
+import { Login } from "../views/Login"
 
 
 const RouterApp = () => {
@@ -8,6 +9,7 @@ const RouterApp = () => {
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
             <Route path="*" element={<NotFound/>}/>
         </Routes>
         </BrowserRouter>
