8b4e8cd065aee1a802fc48351d7bdfe1374ca9e0
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-02-27 09:56:42 -0300

    add: ruta para el NotFound

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { App } from "../components/App"
+import { NotFound } from "../views/NotFound"
+
+
 const RouterApp = () => {
-    <></>
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
+        </BrowserRouter>
+    )
 }
 
 export{ RouterApp }
