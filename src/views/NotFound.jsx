9841c1eb53b54ebb65c24261dee33297facc7f1d
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-02-27 09:35:37 -0300

    add: funcion NotFound

---
+++ src/views/NotFound.jsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+    return(
+        <section className="cont-not-found">
+            <h1>404</h1>
+            <p>Página no encontrada</p>
+            <a href="/">Ir al home</a>
+        </section>
+    )
+}
+
+export { NotFound }
