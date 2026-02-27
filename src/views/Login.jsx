2707de8b659f242465e41858f2729a5d84e5d9a8
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-02-27 10:24:37 -0300

    add: funcion Login con un form

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,6 +1,15 @@
 const Login = () => {
     return(
-        <></>
+        <section>
+            <h2>Bienvenido!! Inicie sesión</h2>
+            <form >
+                <input type="text" placeholder="Ingrese su nombre" />
+                <input type="text" placeholder="Ingrese su apellido"/>
+                <input type="email" placeholder="Ingrese su email"/>
+                <input type="password" placeholder="Ingrese su contraseña"/>
+                <button>Ingresar</button>
+            </form>
+        </section>
     )
 }
 
