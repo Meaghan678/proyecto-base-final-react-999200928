50d432bed1dd14bcbaf1ef348a1154d31ce02c42
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-03-08 16:13:16 -0300

    add: div con la bienvenida en el login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -24,28 +24,36 @@ const Login = () => {
  
         setError(null)
 
-        const response = login({name,email, password})
+        const response = login({email, password})
 
         if(!response){
             setError(true)
             return
         }
 
-        handleUser(name, email, password)
+        handleUser(email, password)
         navigate("/")
     }
 
     return(
-        <section className="login">
-            <h2>Iniciar sesión</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="email" placeholder="Email" onChange={handleChangeEmail}/>
-                <input type="password" placeholder="Contraseña" onChange={handleChangePassword}/>
-                <button>Ingresar</button>
-            </form> 
-            {
-                error && <p className="error-login">Error al ingresar</p>
-            }
+        <section className="container">
+            <div className="welcome">
+                <h2>Bienvenido a Chat UTN</h2>
+                <p>Comienza a chatear con tus amigos de manera gratuita y segura.</p>
+                <p>Para saber mas sobre nosotros entre aqui: </p>
+                <a href="">acerca</a>
+            </div>
+            <div className="login">
+                <h2>Iniciar sesión</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" placeholder="Email" onChange={handleChangeEmail}/>
+                    <input type="password" placeholder="Contraseña" onChange={handleChangePassword}/>
+                    <button>Ingresar</button>
+                </form> 
+                {
+                    error && <p className="error-login">Error al iniciar sesión</p>
+                }
+            </div>
         </section>
     )
 }
