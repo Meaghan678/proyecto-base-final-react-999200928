26e7a77b76e7787143dde6c9a8d7c272c4f1809d
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-03-08 20:17:55 -0300

    add: validacion de errores de los inputs de Register

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -4,6 +4,9 @@ const Register = () => {
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [errorName, setErrorName] = useState(null)
+    const [errorEmail, setErrorEmail] = useState(null)
+    const [errorPassword, setErrorPassword] = useState(null)
 
     const handleChangeNameReg = (e) => {
         setName(e.target.value)
@@ -17,13 +20,47 @@ const Register = () => {
         setPassword(e.target.value)
     }
 
+    const handleSubmitReg = (e) => {
+        e.preventDefault()
+
+        //Validacion Nombre 
+        if(name.length === 0){
+            setErrorName("Campo obligatorio")
+            return
+        }
+        if(name.length < 3){
+            setErrorName("Mínimo 3 caracteres")
+            return
+        }
+
+        //Validacion Email 
+        if(email.length === 0){
+            setErrorEmail("Campo obligatorio")
+            return
+        }
+        if(!email.includes('@')){
+            setErrorEmail("Formato inválido")
+        }
+
+        //Validacion contraseña
+        if(password.length === 0){
+            setErrorPassword("Campo obligatorio")
+        }
+        if(password.length < 6){
+            setErrorPassword("Mínimo 6 caracteres")
+        }
+    }
+
     return (
         <section className="register">
             <h2>Registrate</h2>
-            <form>
-                <input type="text" placeholder="Nombre y apellido" onChange={handleChangeNameReg}/>
+            <form noValidate onSubmit={handleSubmitReg}>
+                <input type="text" placeholder="Nombre y apellido" required onChange={handleChangeNameReg}/>
+                { errorName && <p className="error-name">{errorName}</p>}
                 <input type="email" placeholder="Email" onChange={handleChangeEmailReg}/>
+                { errorEmail && <p className="error-email">{errorEmail}</p>}
                 <input type="password" placeholder="Contraseña" onChange={handleChangePasswordReg}/>
+                { errorPassword && <p className="error-pass">{errorPassword}</p>}
                 <button>Enviar</button>
             </form>
         </section>
